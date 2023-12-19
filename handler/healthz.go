73e202e568b3b921475584ed163ef5ed435f99f5
MinamizonoEno <a02142211@gmail.com>
2023-12-19 16:53:23 +0900

    feat:station5まで

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -11,10 +13,15 @@ type HealthzHandler struct{}
 
 // NewHealthzHandler returns HealthzHandler based http.Handler.
 func NewHealthzHandler() *HealthzHandler {
-	return &HealthzHandler{}
+    return &HealthzHandler{}
 }
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+    response := &model.HealthzResponse{Message: "OK"}
+
+    w.Header().Set("Content-Type", "application/json")
+    if err := json.NewEncoder(w).Encode(response); err != nil {
+        log.Println(err)
+    }
 }
--- handler/router/router.go
+++ handler/router/router.go
@@ -3,10 +3,19 @@ package router
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+
+	// Create a new HealthzHandler
+	healthzHandler := handler.NewHealthzHandler()
+
+	// Register the /healthz endpoint
+	mux.Handle("/healthz", healthzHandler)
+	
 	return mux
 }
